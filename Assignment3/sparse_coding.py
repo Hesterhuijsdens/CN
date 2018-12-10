78f991c84b0115122c53d316de9fa26f1478e833
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2018-12-10 15:51:26 +0100

    PCA components

--- Assignment3/sparse_coding.py
+++ Assignment3/sparse_coding.py
@@ -3,37 +3,20 @@ import matplotlib.image as img
 import matplotlib.pyplot as plt
 from generatePatches import *
 
-patches = generate_data("natural")
+# load data and standardize:
+x = generate_data("natural")
+m = np.mean(x, axis=0)
+x = x - np.ones((np.shape(x)[0], 1)) * m
 
-# visualise patches:
-fig = plt.figure(0)
-for plot_nr in range(1, 65):
-    ax = fig.add_subplot(8, 8, plot_nr)
-    ax.imshow(np.reshape(patches[plot_nr-1, :], (8, 8)), cmap=plt.get_cmap(name='gray'))
+# eigenvalue decomposition:
+eigenvalues, eigvectors = np.linalg.eig(np.cov(np.transpose(x)))
+eigvectors = np.transpose(eigvectors)
+
+# visualize PCA components:
+fig = plt.figure(1)
+for component in range(1, 65):
+    ax = fig.add_subplot(8, 8, component)
+    ax.imshow(np.reshape(eigvectors[component-1, :], (8, 8)), cmap=plt.get_cmap(name='gray'))
     plt.axis('off')
-
-m = np.mean(patches, axis=0)
-patches = patches - np.ones((np.shape(patches)[0], 1)) * m
-
-w = np.random.normal(size=(64, 64))
-max_iteration = 100
-learning_rate = 0.1
-
-#for epoch in range(max_iteration):
-
-
-
-
-
-# visualize PCAs:
-#fig = plt.figure(1)
-# for plot_nr in range(1, 65):
-#     ax = fig.add_subplot(8, 8, plot_nr)
-#     ax.imshow(patches[plot_nr-1, :, :], cmap=plt.get_cmap(name='gray'))
-#     plt.axis('off')
-
-
 plt.show()
-
-
 
